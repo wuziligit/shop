e79f3deff1d79cf3a7688a141f500ff878c5dfca
wuziligit <yilusuixing96@163.com>
2020-08-09 19:53:06 +0800

    修改Test.java文件

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ import innerClass.OuterClass.InnerClass1;
 public class OuterTest {
 public static void main(String[] args) {
 	OuterClass outer  = new OuterClass();
-	//outer.method();
-	//OuterClass.InnerClass inner = new OuterClass().new InnerClass();	
+	outer.method();
+	OuterClass.InnerClass inner = new OuterClass().new InnerClass();	
 }
 }
