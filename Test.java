90dc000498755d82dac8414bedc5d0b5865c5007
wuziligit <yilusuixing96@163.com>
2020-08-09 18:02:15 +0800

    创建一个Test.java文件

---
+++ Test.java
@@ -0,0 +1,11 @@
+package innerClass;
+
+import innerClass.OuterClass.InnerClass1;
+
+public class OuterTest {
+public static void main(String[] args) {
+	OuterClass outer  = new OuterClass();
+	//outer.method();
+	//OuterClass.InnerClass inner = new OuterClass().new InnerClass();	
+}
+}
